23314950b711d49c26dec5f450773af25e039435
se4019 <70657061+se4019@users.noreply.github.com>
2020-11-23 23:20:24 +0900

    12_04

---
+++ main4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int arc, char *argv[]) {
+	
+
+	FILE *fp;
+	char str[100];
+	int i;
+	
+	fp = fopen("sample.txt", "w");
+	if (fp == NULL)
+	{
+		return -1;
+	}
+	
+	for(i=0;i<3;i++)
+	{
+		printf("Input a word : ");
+		scanf("%s", str);
+		fprintf(fp, "%s\n", str);
+	}
+	
+	fclose(fp);
+	
+	return 0;
+	
+	}
